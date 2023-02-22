ba9145de25157253be149ee65b104941fe02e8f1
cloris222 <cloris222@gmail.com>
2023-02-22 23:36:59 +0800

    暫且完成QA

--- src/pages/FrontLayout/QAView.vue
+++ src/pages/FrontLayout/QAView.vue
@@ -17,10 +17,26 @@
           </q-breadcrumbs>
         </div>
       </div>
+
+      <div class="row phone q-mt-lg">
+        <div class="col-12 phone_head bg-primary text-white">
+          <div class="left">
+            <q-icon name="mdi-chevron-left" />
+            99+
+            桌下吧
+          </div>
+          <div class="right">
+            <q-icon name="mdi-magnify" size="md" class="q-mr-sm" />
+            <q-icon name="mdi-phone" size="md" class="q-mr-sm" />
+            <q-icon name="mdi-menu" size="md" />
+          </div>
+        </div>
+      </div>
+
       <!-- Q&A -->
-      <q-parallax :height="1300" :speed="0.5">
+      <q-parallax :height="1800" :speed="0.5">
         <template #media>
-          <img src="@/assets/qa/聊天紀錄-01.svg" class="parallaxImg">
+          <img src="@/assets/qa/qabg7.jpg" class="parallaxImg">
         </template>
 
         <template #content="scope">
@@ -28,7 +44,7 @@
             class="absolute column items-center"
             :style="{
               opacity: 0.8 + (1 - scope.percentScrolled) * 0.55,
-              top: (scope.percentScrolled * 20) + '%',
+              top: (scope.percentScrolled * 30) + '%',
               left: 0,
               right: 0
             }"
@@ -51,7 +67,9 @@
               <div class="col-12">
                 <div class="answer">
                   <div class="answer_text">
-                    目前沒有提供出租。疫情期間建議買新遊戲回家比較安心喔。
+                    目前沒有提供出租。
+                    <br>
+                    疫情期間建議買新遊戲回家比較安心喔。
                   </div>
                   <div class="answer_avatar">
                     <q-avatar>
@@ -78,7 +96,8 @@
               <div class="col-12">
                 <div class="answer">
                   <div class="answer_text">
-                    可以帶蛋糕喔，跟外食一樣垃圾幫忙帶走或酌收$200處理費。另外有冷藏冰箱可以借冰在冰箱。
+                    可以帶蛋糕喔，跟外食一樣垃圾幫忙帶走或酌收$200處理費。
+                    有冷藏冰箱可以借冰在冰箱。
                   </div>
                   <div class="answer_avatar">
                     <q-avatar>
@@ -105,7 +124,8 @@
               <div class="col-12">
                 <div class="answer">
                   <div class="answer_text">
-                    網路預約只提供隔天到60天內的預約，當天要來玩的話可以直接撥店內電話詢問位子喔。
+                    網路預約只提供隔天到60天內的預約，
+                    當天要來玩的話可以直接撥店內電話詢問位子喔。
                   </div>
                   <div class="answer_avatar">
                     <q-avatar>
@@ -133,6 +153,8 @@
                 <div class="answer">
                   <div class="answer_text">
                     不論是要開會、打SWITCH或是喝酒聊天都很適合喔。
+                    <br>
+                    桌下吧隨時歡迎您的到來！
                   </div>
                   <div class="answer_avatar">
                     <q-avatar>
@@ -173,6 +195,31 @@
           </div>
         </template>
       </q-parallax>
+
+      <!-- phone_footer -->
+      <div class="row phone_footer">
+        <div class="col-12  bg-primary text-white">
+          <div class="left">
+            <q-icon name="mdi-plus" size="md" class="q-mr-sm" />
+            <q-icon name="mdi-camera" size="md" class="q-mr-sm" />
+            <q-icon name="mdi-image-area" size="md" />
+          </div>
+          <div class="right">
+            <q-icon name="mdi-microphone" class="q-ml-auto" size="md" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- footer -->
+    <div class="footer bg-primary">
+      <div class="row">
+        <div class="col-12">
+          <div class="copyright">
+            Copyright © 2023 Yung Chien Yang. All rights reserved.
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
