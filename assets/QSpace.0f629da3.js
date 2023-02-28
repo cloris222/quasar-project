cf38842a4f9baa25ba64c68eb5acdee4bf60ad61
cloris222 <113668331+cloris222@users.noreply.github.com>
2023-02-28 11:50:33 +0000

    Deploying to gh-pages from @ cloris222/quasar-project@3db3e649a34e4e586dcc67504ca4f966a138a0fc 🚀

---
+++ assets/QSpace.0f629da3.js
@@ -0,0 +1,1 @@
+import{h as e,k as a}from"./index.9613f6f6.js";const p=e("div",{class:"q-space"});var c=a({name:"QSpace",setup(){return()=>p}});export{c as Q};
---
+++ assets/swiper.940f2121.js
@@ -0,0 +1,1 @@
+import{f as r,S as s,j as a}from"./index.9613f6f6.js";var o=r(({app:e})=>{e.use(s),e.use(a)});export{o as default};
