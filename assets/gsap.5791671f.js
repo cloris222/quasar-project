b72d15698f07fae8603c2cff31042fadc6d888b3
cloris222 <113668331+cloris222@users.noreply.github.com>
2023-03-01 15:33:03 +0000

    Deploying to gh-pages from @ cloris222/quasar-project@03ca8d9f6d79578191b772ec0948006af92c6dc5 🚀

---
+++ assets/gsap.5791671f.js
@@ -0,0 +1,1 @@
+import{f as s,j as o}from"./index.d0c8509c.js";var p=s(async({app:a})=>{a.use(o)});export{p as default};
---
+++ assets/swiper.4118ad0e.js
@@ -0,0 +1,1 @@
+import{f as r,S as s,k as a}from"./index.d0c8509c.js";var o=r(({app:e})=>{e.use(s),e.use(a)});export{o as default};
